2668f9c533979c91d41f2449c8cbabba88ce92f3
Betsi Loayza <betsi18_12@hotmail.com>
2017-03-06 17:00:46 -0500

    coders

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -23,3 +23,27 @@ window.onclick = function(event) {
 		}
 	}
 }
+
+var arrayFicha = [ { nombre : "Meche", apellido : "Zubieta", promocion : "1era Promoción", estado : "Trabajando" },
+{ nombre : "Arabela", apellido : "Rojas", promocion : "2da Promoción", estado : "Trabajando" },
+{ nombre : "Maria", apellido : "Rosán", promocion : "2da Promoción", estado : "Trabajando" },
+{ nombre : "Guadalupe", apellido : "Racio", promocion : "3ra Promoción", estado : "Trabajando" },
+{ nombre : "Sara", apellido : "Casa", promocion : "3ra Promoción", estado : "Trabajando" },
+{ nombre : "Daniella", apellido : "Durán", promocion : "4ra Promoción", estado : "Trabajando" },
+{ nombre : "Michelle", apellido : "Seguil", promocion : "5ra Promoción", estado : "Trabajando"},
+{ nombre : "Diana", apellido : "Navarro", promocion : "5ra Promoción", estado : "Trabajando" } ];
+
+
+var imprimir=document.getElementById('imprimir');
+
+var allFicha = "";
+var array = [0,1,2,3,4,5,6,7]
+  arrayFicha.forEach(function(e,i){
+    allFicha =  "Nombre: "+ e.nombre + "</br>" +
+                       "Apellido: "+ e.apellido + "</br>" +
+                       "Promoción: "+ e.promocion + "</br>" +
+                       "Estado: "+ e.estado + "</br></br>";
+var indice = array[i];
+var indice = document.getElementById(i)
+indice.innerHTML = allFicha;
+  })
