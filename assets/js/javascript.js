ffcd3066278d737be2c68475dcb8d477e30fff4d
Betsi Loayza <betsi18_12@hotmail.com>
2017-03-06 16:59:24 -0500

    dropdown

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,3 +6,20 @@ var mostarOcultar = function(divtexte) {
     divtext.style.display = "block";
   }
 }
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
